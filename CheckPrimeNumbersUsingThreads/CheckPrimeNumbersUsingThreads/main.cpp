d88e065790d62533a72d21fc455fca2dfb42ca9f
Kirill Sivograkov <110250397+KirSivogr@users.noreply.github.com>
2022-12-29 22:57:26 +0300

    final update

--- CheckPrimeNumbersUsingThreads/CheckPrimeNumbersUsingThreads/main.cpp
+++ CheckPrimeNumbersUsingThreads/CheckPrimeNumbersUsingThreads/main.cpp
@@ -4,32 +4,34 @@
 #include <mutex>
 using namespace std;
 
+vector <bool> p;
+int ans = 0;
 int k = 0;
 int t;
 mutex mtx;
 
-bool isPrime(int n){
-	if (n == 0 || n == 1) {
-		return false;
-	}
-	for(int i = 2;i <= sqrt(n) ; i++) {
-		if (n % i == 0) {
-			return false;
-		}
-	}	
-	return true;
+bool isPrime(int n) {
+    if (n == 0 || n == 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-void PrimeCheck(vector <int>& a, vector <bool>& p) {
+void PrimeCheck(vector <int>& a) {
     while (true) {
         unique_lock<mutex> ul(mtx);
         if (k == a.size()) {
             return;
         }
-    	k++;
-    	t = k;
+        k++;
+        t = k;
         ul.unlock();
-        if(isPrime(vec[t - 1]))
+        if (isPrime(a[t - 1]))
             p[t - 1] = 1;
     }
 }
@@ -37,22 +39,22 @@ int main() {
     int n;
     cin >> n;
     vector <int> a(n);
-    vector <bool> p(n);
-    for (int i = 0; i < vec.size(); i++) {
+    p.resize(n);
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
     int sz;
     cin >> sz;
     vector <thread> th;
     for (int i = 0; i < sz; i++) {
-        th.emplace_back(PrimeCheck, ref(vec), ref(res));
+        th.emplace_back(PrimeCheck, ref(a));
     }
     for (auto& i : th) {
         i.join();
     }
-    for (int i = 0; i < res.size(); i++) {
-       	if (p[i]) {
-		cout <<a[i] <<" is prime" <<endl;
-	}
+    for (int i = 0; i < n; i++) {
+        if (p[i]) {
+            cout << a[i] << " is prime" << endl;
+        }
     }
 }
